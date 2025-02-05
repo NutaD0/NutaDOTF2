ac9072b8b01966f5b21cb4bc64c5848243650f3b
Amir Salimov <nutado2005@gmail.com>
2025-02-05 14:04:08 +0600

    first commit

---
+++ script/script.js
@@ -0,0 +1,27 @@
+let discordButton = document.getElementById("dsBtn");
+let telegramButton = document.getElementById("tgBtn");
+let tiktokButton = document.getElementById("ttBtn");
+let youtubeButton = document.getElementById("ytBtn");
+let myStuffButton = document.getElementById("myStuffBtn");
+
+discordButton.addEventListener("click", () => {
+  window.open("https://discord.gg/dadrKcewXh");
+});
+
+telegramButton.addEventListener("click", () => {
+  window.open("https://t.me/nutadotf2");
+});
+
+tiktokButton.addEventListener("click", () => {
+  window.open("https://www.tiktok.com/@nutadotf2");
+});
+
+youtubeButton.addEventListener("click", () => {
+  window.open("https://www.youtube.com/@nutadotf2");
+});
+
+myStuffButton.addEventListener("click", () => {
+  window.open(
+    "https://drive.google.com/file/d/1D1n_XuWM0qceECNP4yJ7BqxoUfs2deO1/view?usp=sharing"
+  );
+});
