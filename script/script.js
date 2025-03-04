ea725c8bad90cddde2bfb5bc8918966242b388ee
daniil <pomazkov_d04@mail.ru>
2025-03-04 21:42:24 +0500

    отслеживание активности

--- script/script.js
+++ script/script.js
@@ -26,23 +26,3 @@ myStuffButton.addEventListener("click", () => {
   );
 });
 
-useHead({   
-  script: [
-      {
-          async:true, 
-          src: 'https://www.googletagmanager.com/gtag/js?id=G-WTH45PLZMZ',
-      },
-      {
-          innerHTML: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js',new Date());
-          gtag('config', 'G-WTH45PLZMZ');
-          `,
-          type: 'text/javascript',
-      },
-  ],
-  __dangerouslyDisableSanitizersByTagID: {
-      gtag: ['innerHTML'],
-  },
-});
